09669f25df124744121916a8eebabdec84482aec
Breken Davis and Ian Alexander <breken@>
2019-06-03 20:44:49 -0700

    Updates and additions to original file including age, height, form and converted the alert to an embedded portion of html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
-  var height = parseInt(prompt("How tall are you?"));
-  $('#shortys').show();
-  $('#regularRudys').show();
-  $('#sasquatch').show();
-    if (height <= 3) {
-    // $('#shortys').show();
-    $("#shortys").addClass("highlight");
-  } else if (height > 3 && height <= 5) {
-    // $('#regularRudys').show();
+$('form#rides').submit(function(event) {
+  var height = parseInt($('input#height').val());
+  var age = parseInt($('input#age').val());
+
+  // $('#shortys').show();
+  // $('#regularRudys').show();
+  // $('#sasquatch').show();
+    if (height <= 3 && age <=10){
+    $('#shortys').show();
+    // $("#shortys").addClass("highlight");
+  } else if (height > 3 && height <= 5 || age > 18) {
+    $('#regularRudys').show();
 
   } else {
-    // $('#sasquatch').show();
+    $('#sasquatch').show();
   }
+   event.preventDefault();
+});
 });
 
 // $(document).click(function(list) {
